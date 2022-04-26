854fbb1e64958b7bb4c1e21c8565e82e7d1ffc42
Adrián Jiménez <halteradri14@gmail.com>
2022-04-26 18:44:42 +0200

    create urlshortener API

---
+++ boilerplate-project-urlshortener-gomix/server.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const urlParser = require('url')
+const dns = require('dns')
+const Schema = mongoose.Schema
+
+// Basic Configuration
+
+const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({extended:false}))
+
+const URL_SCHEMA = new Schema({
+  original_url: {type:String, unique: true},
+  short_url: {type: Number, unique: true}
+})
+
+var urlModel = mongoose.model('urlModel', URL_SCHEMA)
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res)=>{
+   const urlValid = dns.lookup(urlParser.parse(req.body.url).hostname, (err, doc)=>{
+    if(!doc){
+      res.json({error: 'Invalid url'})
+    }
+    else{
+      const MY_URL = new urlModel({original_url: req.body.url})
+      MY_URL.save((err,doc)=>{
+        res.json({
+          original_url: doc.original_url,
+          short_url: doc.id
+        })
+      })
+    }
+  })
+})
+
+app.get('/api/shorturl/:id', (req,res)=>{
+  const id = req.params.id
+  urlModel.findById(id, (err,doc)=>{
+    if(err) console.log(err)
+    if(!doc){
+      res.json({error: 'Invalid id'})
+    }
+    else{
+      res.redirect(doc.original_url)
+    }
+  })
+})
+
+
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
